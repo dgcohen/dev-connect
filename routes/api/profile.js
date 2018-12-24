db3f38c918c95bbad021f826e4b002b9280ccd31
Diego Cohen <diegocohen@Diegos-MBP-2.fios-router.home>
2018-12-24 12:53:46 -0500

    Add routes for getting profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 
@@ -19,6 +22,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -30,6 +34,62 @@ router.get(
   }
 );
 
+// @route     GET api/profile/all
+// @desc      Get all profiles
+// @access    Public
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
+// @route     GET api/profile/handle/:handle
+// @desc      Get profile by handle
+// @access    Public
+
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get profile by user id
+// @access    Public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route     POST api/profile
 // @desc      Create or edit user profile
 // @access    Private
@@ -38,6 +98,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
     // Get fields
 
     const profileFields = {};
@@ -93,9 +161,9 @@ router.post(
   }
 );
 
-// @route     Get api/profile/test
-// @desc      Tests profile route
-// @access    Public
+// @route     Get api/profile/experience
+// @desc      Add experience to profile
+// @access    Private
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 
 module.exports = router;
