381353aba7f38056539925ab4b910fc840220a6f
Diego Cohen <diego.cohen3@gmail.com>
2019-01-06 17:02:44 -0500

    Create education display table

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -54,15 +54,17 @@ export const addExperience = (expData, history) => dispatch => {
     );
 };
 
-// Delete experience
+// Delete Experience
 
 export const deleteExperience = id => dispatch => {
   axios
     .delete(`/api/profile/experience/${id}`)
-    .then(res => dispatch => ({
-      type: GET_PROFILE,
-      payload: res.data
-    }))
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
@@ -77,6 +79,25 @@ export const addEducation = (eduData, history) => dispatch => {
   axios
     .post("/api/profile/education", eduData)
     .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Education
+
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
