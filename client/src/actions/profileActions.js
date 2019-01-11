58b193ee29d4863fcd8ccea8ad13859a042af457
Diego Cohen <diegocohen@Diegos-MBP-2.home>
2019-01-10 22:56:55 -0500

    Initialize Profile component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -28,6 +28,25 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
@@ -112,7 +131,7 @@ export const deleteEducation = id => dispatch => {
 export const getProfiles = () => dispatch => {
   dispatch(setProfileLoading());
   axios
-    .get('/api/profile/all')
+    .get("/api/profile/all")
     .then(res =>
       dispatch({
         type: GET_PROFILES,
@@ -126,7 +145,6 @@ export const getProfiles = () => dispatch => {
       })
     );
 };
-
 
 // Delete account & profile
 
